f7e4af2a2f8a8190ce7df464911438fc34a163f6
Mynul Alam <mynulalam27@gmail.com>
2025-06-13 01:16:02 +0600

    c

--- lib/auth service/authentication.dart
+++ lib/auth service/authentication.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+//database with auhtetication
 
 class AuthServices {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
